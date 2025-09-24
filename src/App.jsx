4c7528a532ef9937f6824ad62d3c814a50fdd931
MarcosFig-dev <156377579+MarcosFig-dev@users.noreply.github.com>
2025-09-24 13:21:17 +1200

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from "./pages/HomePage.jsx"
-import Multiplayer from './pages/Multiplayer.jsx';
+import React from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import Multiplayer from "./pages/Multiplayer.jsx";
 
 function App() {
   return (
     <div className="app">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="Multiplayer" element={<Multiplayer />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
